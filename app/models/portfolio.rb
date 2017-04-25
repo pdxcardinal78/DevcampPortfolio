e52baef369cd42656855325e4b8f53da552d6c2e
RYAN J JOHNSON <Ryan_Johnson@RYANs-MacBook-Pro.local>
2017-04-24 21:44:10 -0700

    added data defaults for portfolio images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,8 +1,15 @@
 class Portfolio < ApplicationRecord
-	validates_presence_of :title, :subtitle, :body, :main_image
+	validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
 
 	def self.angular
 		where(subtitle: 'Angular')
-
 	end
+
+after_initialize :set_defaults
+
+def set_defaults
+	self.main_image ||= "http://placehold.it/600x400"
+	self.thumb_image ||= "http://placehold.it/350x200"
+end
+
 end
