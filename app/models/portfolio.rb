be2ca51a1fdce0044a61bf33ae607a45bda49796
RYAN J JOHNSON <Ryan_Johnson@RYANs-MacBook-Pro.local>
2017-05-16 23:19:37 -0700

    added initial file uploading system via carrierwaves for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,6 +5,9 @@ class Portfolio < ApplicationRecord
 	include Placeholder
 	validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
 
+	mount_uploader :thumb_image, PortfolioUploader
+	mount_uploader :main_image, PortfolioUploader
+
 	def self.angular
 		where(subtitle: 'Angular')
 	end
@@ -12,6 +15,7 @@ class Portfolio < ApplicationRecord
 	def self.by_position
 		order("position ASC")
 	end
+
 
 
 after_initialize :set_defaults
