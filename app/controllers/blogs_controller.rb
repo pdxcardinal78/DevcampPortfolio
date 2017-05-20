dbcfa70a6d873f79b5fb4e4bf3581fef1a7c9fdf
RYAN J JOHNSON <Ryan_Johnson@RYANs-MacBook-Pro.local>
2017-05-19 23:21:36 -0700

    built out intial form and partial call for comments and action call

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -13,6 +13,11 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
+    @blog = Blog.includes(:comments).friendly.find(params[:id])
+    @comment = Comment.new
+
+    @page_title = @blog.title
+    @seo_keywords = @blog.body
   end
 
   # GET /blogs/new
@@ -72,7 +77,7 @@ class BlogsController < ApplicationController
     elsif @blog.published?
       @blog.draft!
     end
-    
+
     redirect_to blogs_url notice: 'Blog was successfully destroyed.'
   end
 
